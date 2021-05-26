c4995a5d491533f5ad5501363e2e131a71b6c0ef
ashaase <ashaase123@gmail.com>
2021-05-25 20:15:29 -0500

    player updates

--- esports-guides/src/app/player/player.component.ts
+++ esports-guides/src/app/player/player.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Input, Component, OnInit } from '@angular/core';
 import { Player, Game, Team } from './player.model';
 @Component({
   selector: 'app-player',
@@ -6,12 +6,29 @@ import { Player, Game, Team } from './player.model';
   styleUrls: ['./player.component.css']
 })
   export class PlayerComponent implements OnInit {
+    @Input() player: Player;
     dataColumns: string[] = ['mouse', 'keyboard', 'headset', 'monitor', 'sensitivity'];
     dataSource = PLAYER_DATA;
 
+    name: string;
+    game: Game;
+    team: Team;
+    mouse: string;
+    keyboard: string;
+    headset: string;
+    monitor: string;
+    sensitivity: string;
+
     constructor() { }
 
-
+    names: string[] = ['Perkz', 'Zven'];
+    games: string[] = ['League of Legends', 'Rocket League'];
+    teams: string[] = ['Cloud9', 'TSM'];
+    mice: string[] = ['Razer DeathAdder'];
+    keyboards: string[] = ['Corsair K95'];
+    headsets: string[] = ['Logitech G95'];
+    monitors: string[] = ['Dell AW5125'];
+    sensitivities: string[] = ['1000, 4, 25'];
 
     ngOnInit(): void {
     }
@@ -22,5 +39,3 @@ import { Player, Game, Team } from './player.model';
   const PLAYER_DATA: Player[] = [
     {mouse: "Razer DeathAdder", keyboard: "Corsair K95", headset: "Logitech G95", monitor: "Dell AW5125", sensitivity: "1000, 4, 25", name: "Alex", team: Team.Cloud9, game: Game.RocketLeague}
     ];
-
-
--- esports-guides/src/app/teams/teams.component.ts
+++ esports-guides/src/app/teams/teams.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Player, Team, Game } from '../player/player.model';
+
 @Component({
   selector: 'app-teams',
   templateUrl: './teams.component.html',
@@ -7,7 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TeamsComponent implements OnInit {
 
-  constructor() { }
+  players: Player[];
+
+  constructor() {
+    this.players = [new Player('Perkz', Game.LeagueOfLegends, Team.Cloud9, 'Razer DeathAdder', 'Corsair K95', 'Logitech G95',
+                                'Dell AW5125', '1000, 4, 25')];
+  }
 
   ngOnInit(): void {
   }

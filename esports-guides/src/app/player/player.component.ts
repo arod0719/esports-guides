224387c026c6dec7cab38d8830caac57fea83cb5
ashaase <ashaase123@gmail.com>
2021-05-25 19:22:36 -0500

    player model3

--- esports-guides/src/app/player/player.component.ts
+++ esports-guides/src/app/player/player.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Player, Game, Team } from './card.model';
+import { Player, Game, Team } from './player.model';
 
   export interface PlayerData {
     mouse: string;
